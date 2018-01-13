11e41d8a532b675dde23cdfe539ec448f19dbb32
Max J. Polster <max.polster@id-p.de>
2018-01-13 03:17:03 +0100

    Initial project.

---
+++ README.md
@@ -0,0 +1,27 @@
+# matchscore
+Configurable fuzzy string matching and scoring
+
+## Installation
+```js
+npm install matchscore
+```
+
+## Usage
+```js
+const score = require('matchscore');
+
+score('Hello World!', 'world'); // -> 1
+score('Hello World!', 'lll'); // ~> 0.745
+```
+
+### Explaination
+The returned score is a number between 0 and 1 where 0 stands for no match and 1 for a complete contiguous occurrence of the query strings in the searched string. The more the query string is divided, the lower the score.
+
+### Configuration
+The config function returns a different score function with the custom configuration applied.
+The configuration below is the default.
+```js
+const score = require('matchscore').config({
+	ignoreCase: true
+});
+```
---
+++ test/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const test = require('ava');
+const score = require('..');
+
+test('Full match', t => {
+	t.is(score('abc', 'ab'), 1);
+	t.is(score('abc', 'bc'), 1);
+	t.is(score('abc', 'abc'), 1);
+});
+
+test('Fuzzy match', t => {
+	t.true(score('abc', 'ac') > 0);
+	t.true(score('abc', 'ac') < 1);
+});
+
+test('Coherent exponential score', t => {
+	t.true(score('01234', '014') > score('01234', '024'));
+});
+
+test('Config', t => {
+	t.is(score.config({ignoreCase: false})('abc', 'ABC'), 0);
+	t.is(score.config({ignoreCase: true})('abc', 'ABC'), 1);
+});
+
+test('Config defaults', t => {
+	const ignoreCase = true;
+	t.is(score('abc', 'ABC'), ignoreCase ? 1 : 0);
+});
+
+test('Empty query', t => {
+	t.is(score('abc', ''), 0);
+	t.is(score('', ''), 0);
+});
+
+console.log(score('bacd', 'bc'));
